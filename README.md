33b103d94b9d5c69fe34f1dcac6afb8185b3ccee
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2017-06-27 12:00:36 +0200

    make it work with different canteens

--- README.md
+++ README.md
@@ -1,10 +1,33 @@
 dmensamenu
 ==========
 
-Print Heidelberg Im Neuenheimer Feld canteen menu using `dmenu`.
+Print Heidelberg canteen menus using `dmenu`.
 The menu is fetched from http://stw.uni-heidelberg.de/de/speiseplan.
+
+Requirements
+------------
+
+Of course, this requires dmenu to be installed.
+If you wish to use it with something else like Rofi, send in a pull request.
+
+Installation
+------------
+
+    $ git clone https://github.com/dotlambda/dmensamenu.git
+    $ cd dmensamenu
+    $ pip install --user .
 
 Usage
 -----
 
-    $ ./dmensamenu
+    $ dmensamenu canteen-name
+
+where *canteen-name* is one of
+
+* INF
+* Marstall
+* Triplex
+
+You can even pass extra arguments to dmenu:
+
+    $ dmensamenu Marstall -nb \#101b2a -nf \#dcdccc -sb \#dcdccc -sf \#101b2a -fn "Fira Mono"
--- dmensamenu/__init__.py
+++ dmensamenu/__init__.py
@@ -1,37 +1,62 @@
+import sys
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime, date, timedelta
 from subprocess import Popen, PIPE
 
-def getmeals(day):
+CANTEENS = {
+    'INF': 'Mensa Im Neuenheimer Feld 304',
+    'Marstall': 'zeughaus-Mensa im Marstall',
+    'Triplex': 'Triplex-Mensa am Uniplatz'
+}
+
+CLOSING = {
+    'INF': 14,
+    'Marstall': 22,
+    'Triplex': 14
+}
+
+def getmeals(canteen, day):
     meals = []
+    maxoutlen = 0
+    maxpricelen = 0
+
     page = requests.get('http://stw.uni-heidelberg.de/de/speiseplan').text
     html = BeautifulSoup(page, 'html.parser')
-    h2 = html.body.find('h2', text='Mensa Im Neuenheimer Feld 304')
+    h2 = html.body.find('h2', text=CANTEENS[canteen])
     for h4 in h2.find_next_sibling('div').find_all('h4'):
         if datetime.strptime(h4.text.split(' ')[1], '%d.%m.%Y').date() == day:
             for tr in h4.next_sibling.find_all('tr'):
                 tds = tr.find_all('td')
                 if tds:
                     meal = ' '.join(tds[0].text.split()) # replace any whitespaces by a single ' '
-                    output = tds[1].text
-                    price = tds[2].text
-                    text = output
-                    text += ' ' * (5 - len(text))
-                    text += price
-                    text += ' ' * (12 - len(text))
-                    text += meal
-                    meals.append(text)
-    return meals
+                    out = tds[1].text
+                    price = tds[2].text + '€'
 
-def showmeals(meals):
-    p = Popen(['dmenu', '-l', str(len(meals))], stdin=PIPE)
+                    maxoutlen = max(maxoutlen, len(out))
+                    maxpricelen = max(maxpricelen, len(price))
+
+                    meals.append([out, price, meal])
+
+    for meal in meals:
+        meal[0] += ' ' * (maxoutlen - len(meal[0]))
+        meal[1] += ' ' * (maxpricelen - len(meal[1]))
+    return ['  '.join(meal) for meal in meals]
+
+def showmeals(canteen, day, meals):
+    # remaining command line args are passed to dmenu
+    p = Popen(['dmenu', '-p', canteen + ' ' + day.strftime('%d.%m.%Y'), '-l', str(len(meals))] + sys.argv[2:], stdin=PIPE)
     p.communicate(input=str.encode('\n'.join(meals)))
 
 def main():
+    canteen = sys.argv[1]
+
     daytoshow = date.today()
-    if (datetime.now().hour >= 14): # canteen closes at 14:00, show tomorrow's menu instead
+    if datetime.now().hour >= CLOSING[canteen]: # if canteen is already closed, show tomorrow's menu instead
         daytoshow += timedelta(days=1)
 
-    meals = getmeals(daytoshow)
-    showmeals(meals)
+    meals = getmeals(canteen, daytoshow)
+    showmeals(canteen, daytoshow, meals)
+
+if __name__ == '__main__':
+    main()
