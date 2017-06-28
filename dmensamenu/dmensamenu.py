105005748e478e16121b9a7e94a3a6435c18a76c
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2017-06-28 09:39:10 +0200

    allow selection from given list of canteens

--- dmensamenu/dmensamenu.py
+++ dmensamenu/dmensamenu.py
@@ -45,12 +45,17 @@ def showmenu(menu, dmenu):
     p = Popen(dmenu, stdout=PIPE, stdin=PIPE, shell=True, env=env)
     selected, err = p.communicate(input=str.encode('\n'.join(menu)))
 
-def getcanteens():
+def getcanteens(ids):
     canteens = []
     nextlink = 'http://openmensa.org/api/v2/canteens?page=1'
     while True:
         req = requests.get(nextlink)
-        canteens += req.json()
+        if ids:
+            for canteen in req.json():
+                if canteen['id'] in ids:
+                    canteens.append(canteen)
+        else:
+            canteens += req.json()
 
         links = requests.utils.parse_header_links(req.headers['link'])
         nextlink = None
@@ -75,24 +80,29 @@ def showcanteens(canteens, dmenu, printid=False):
             return canteen['id']
 
 def main():
-    parser = argparse.ArgumentParser(description='Show today\'s canteen menu.',
-                                     formatter_class=RawTextHelpFormatter)
-    parser.add_argument('id', type=int, nargs='?',
-                        help='openmensa.org canteen ID'
-                            +'\nIf no ID is given, a menu for selecting from available canteens is shown.')
-    parser.add_argument('--search', action='store_true',
-                        help='search for a canteen'
+    parser = argparse.ArgumentParser(description='Show today\'s canteen menu.', add_help=False)
+    parser.add_argument('id', type=int, nargs='*',
+                        help='Openmensa.org canteen ID.'
+                            +' If no ID is given, a menu for selecting from available canteens is shown.'
+                            +' If multiple IDs are given, only those will be available for selection.')
+    parser.add_argument('-s', '--search', action='store_true',
+                        help='Search for a canteen.'
                             +'\nThe selected canteen\'s ID will be printed to stdout.')
     parser.add_argument('--dmenu', metavar='cmd', default='dmenu -i -l $lines',
-                        help='command to execute instead of \'dmenu -i -l $lines\''
-                            +'\nCan be used to pass custom parameters to dmenu.'
+                        help='Command to execute instead of \'dmenu -i -l $lines\'.'
+                            +' Can be used to pass custom parameters to dmenu.'
                             +' The shell variable $lines will be set to the number of items on the menu.')
+    parser.add_argument('-v', '--version', action='version',
+                        version='dmensamenu-0.0.1', help="Show version number and exit.")
+    parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
+                        help='Show this help message and exit.')
     args = parser.parse_args()
 
-    canteen = args.id
-    if args.id is None:
-        canteens = getcanteens()
+    if len(args.id) != 1:
+        canteens = getcanteens(args.id)
         canteen = showcanteens(canteens, args.dmenu, args.search)
+    else:
+        canteen = args.id[0]
     if canteen is not None and not args.search:
         meals = getmenu(canteen)
         menu = formatmenu(meals)
