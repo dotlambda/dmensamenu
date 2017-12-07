7b6ad2057975c171956a3f307bb26bf13f7b9e2c
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2017-12-07 12:17:28 +0100

    Add --city argument

--- dmensamenu/dmensamenu.py
+++ dmensamenu/dmensamenu.py
@@ -1,5 +1,4 @@
 import argparse
-import re
 from datetime import datetime
 import requests
 import os
@@ -45,17 +44,14 @@ def showmenu(menu, dmenu):
     p = Popen(dmenu, stdout=PIPE, stdin=PIPE, shell=True, env=env)
     selected, err = p.communicate(input=str.encode('\n'.join(menu)))
 
-def getcanteens(ids):
+def getcanteens(ids=[], city=None):
     canteens = []
     nextlink = 'http://openmensa.org/api/v2/canteens?page=1'
     while True:
         req = requests.get(nextlink)
-        if ids:
-            for canteen in req.json():
-                if canteen['id'] in ids:
-                    canteens.append(canteen)
-        else:
-            canteens += req.json()
+        for canteen in req.json():
+            if (not ids or canteen['id'] in ids) and (city is None or canteen['city'].lower() == city.lower()):
+                canteens.append(canteen)
 
         links = requests.utils.parse_header_links(req.headers['link'])
         nextlink = None
@@ -81,7 +77,7 @@ def showcanteens(canteens, dmenu, printid=False):
 
 def main():
     parser = argparse.ArgumentParser(description='Show today\'s canteen menu.', add_help=False)
-    parser.add_argument('id', type=int, nargs='*',
+    parser.add_argument('ID', type=int, nargs='*',
                         help='Openmensa.org canteen ID.'
                             +' If no ID is given, a menu for selecting from available canteens is shown.'
                             +' If multiple IDs are given, only those will be available for selection.')
@@ -91,7 +87,10 @@ def main():
     parser.add_argument('-s', '--search', action='store_true',
                         help='Search for a canteen.'
                             +'\nThe selected canteen\'s ID will be printed to stdout.')
-    parser.add_argument('--dmenu', metavar='cmd', default='dmenu -i -l $lines',
+    parser.add_argument('--city',
+                        help='When searching for a canteen, only show the ones from the city specified'
+                            +' (case-insensitive).')
+    parser.add_argument('--dmenu', metavar='CMD', default='dmenu -i -l $lines',
                         help='Command to execute instead of \'dmenu -i -l $lines\'.'
                             +' Can be used to pass custom parameters to dmenu.'
                             +' The shell variable $lines will be set to the number of items on the menu.')
@@ -101,8 +100,8 @@ def main():
                         help='Show this help message and exit.')
     args = parser.parse_args()
 
-    if len(args.id) != 1:
-        canteens = getcanteens(args.id)
+    if len(args.ID) != 1:
+        canteens = getcanteens(args.ID, args.city)
         canteen = showcanteens(canteens, args.dmenu, args.search)
     else:
         canteen = args.id[0]
